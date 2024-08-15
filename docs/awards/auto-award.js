8d2c4162858598383be1a8580e29c215d977996a
jabu10245 <jabu.10245@gmail.com>
2024-08-15 14:38:49 +0200

    added @ThePS1Addict to enable !!thepsone

--- docs/awards/auto-award.js
+++ docs/awards/auto-award.js
@@ -24,6 +24,10 @@ const soundCommands = [
     {
         usernames: ['nottheps1addict'],
         sounds: ['namispep'],
+    },
+    {
+        usernames: ['theps1addict'],
+        sounds: ['thepsone'],
     }
 ];
 
